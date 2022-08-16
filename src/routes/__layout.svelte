328f2532d474345fbd73ddeab816a1d8e4cc6bda
chicodevbr <chicodevbr@gmail.com>
2022-08-16 16:32:08 -0300

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>Auth</title>
+</svelte:head>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/auth/login">Login</a>
+	<a href="/auth/register">Register</a>
+
+	<a href="/protected">Admin</a>
+	<a href="/auth/logout">Logout</a>
+</nav>
+
+<slot />
